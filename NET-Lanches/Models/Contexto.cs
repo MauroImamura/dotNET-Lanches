c69b88f2af714d1cdd07b79e3fec516f406cbc73
MauroT <mauro.imamura@outlook.com>
2022-06-05 03:16:47 -0300

    Add project files.

---
+++ NET-Lanches/Models/Contexto.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace NET_Lanches.Models
+{
+    public class Contexto : DbContext
+    {
+        public Contexto(DbContextOptions<Contexto> options) : base (options)
+        {
+
+        }
+
+        public DbSet<Categoria> Categoria { get; set; }
+        public DbSet<Lanche> Lanche { get; set; }
+    }
+}
---
+++ NET-Lanches/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NET_Lanches.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
